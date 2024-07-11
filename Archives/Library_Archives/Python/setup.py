f511c51aec7eeaecd29afb34dd59e9e3add1b51c
Uncover-F <120447843+Uncover-F@users.noreply.github.com>
2024-07-11 05:09:02 +0000

    sdf

--- Archives/Library_Archives/Python/setup.py
+++ Archives/Library_Archives/Python/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='TrAS',
     packages=find_packages(include="TrAS"),
-    version='0.1.0',
+    version='0.1.1',
     description='Translation Utilities',
     author='Circuitbreaker08, Uncover',
     author_email="circuitbreaker08@gmail.com",
